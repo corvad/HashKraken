0c76f77a65f4816b9252e83ea62c713e6cd76eb4
corvad <100031399@ccisd.net>
2022-10-31 10:49:22 -0500

    Reading in array

--- src/Hash.java
+++ src/Hash.java
@@ -1,8 +1,15 @@
-public class Hash {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Hash<priavte> {
     private String path;
     private String hash;
     private int threads;
     private String[] possible;
+    private rainbow
+    private HashMap<String,String> rainbow = new HashMap<>();
 
     public Hash(String hash, int threads, String path){
         this.path = path;
@@ -15,7 +22,24 @@ public class Hash {
         return path;
     }
 
-    priavte String fileRead(){
+    private void fileRead(){
+        // Read Dictionary into Memory.
+        try {
+            possible = Files.readAllLines(Paths.get(path)).toArray(new String[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String start(){
+        // Create New Hashing Threads.
 
     }
+
+    public void stop(){
+        // Stop all threads.
+
+    }
+
+
 }
