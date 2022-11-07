991355259da66b54e862809e3d4df090ffc31271
corvad <100031399@ccisd.net>
2022-11-07 10:47:04 -0600

    Changed JDK Version to 17 from 13

--- src/HashKraken.java
+++ src/HashKraken.java
@@ -4,9 +4,12 @@ public class HashKraken {
 
     public static void main(String[] args){
         // Check CMD arguments
+
+
         if(args[0].equals("--help") || args[0].equals("--h")){
             // Add Later
         }
+
         System.out.println(Arrays.toString(args));
 
 
