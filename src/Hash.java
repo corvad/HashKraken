bee70da08dd3a9b718633de8fd8d73e7d83642da
corvad <71835941+corvad@users.noreply.github.com>
2022-11-09 21:14:28 -0600

    Added fix to thread array creation. Added MD5 Algorithm with check.

--- src/Hash.java
+++ src/Hash.java
@@ -1,15 +1,24 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.HexFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Hash{
     private String path;
-    private String hash;
+    protected String hash;
     private int threads;
-    private String[] possible;
+    protected String[] possible;
     private boolean createHashMap;
+
     private HashMap<String,String> rainbow = new HashMap<>();
+
+
 
     public Hash(String hash, int threads, String path, boolean createHashMap){
         this.path = path;
@@ -34,31 +43,37 @@ public class Hash{
 
     public void start(){
         // Check If Hashmap Dictionary For Hash Algorithm Exists
-
         // Start Threads to Hash Passwords and Split Array Indexes Into Equal Parts
         int max = possible.length-1;
         int lengths = max/threads;
         int indexMax = lengths;
         int indexMin = 0;
         for(int x=0; x<threads;x++){
-            indexMax+=lengths;
-            indexMin+=lengths+1;
+            if(x!=0){
+                indexMax+=lengths;
+                indexMin+=lengths+1;
+            }
             int finalIndexMin = indexMin;
             int finalIndexMax = indexMax;
             new Thread(() -> {
-                hashAlgorithm(finalIndexMin, finalIndexMax);
-            });
+                try {
+                    hashAlgorithm(finalIndexMin, finalIndexMax);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
         }
+
+    }
+
+
+    private void hashAlgorithm(int min, int max) throws NoSuchAlgorithmException {
+        System.out.println("Hello");
     }
 
     public void stop(){
         // Stops Program Execution
         System.exit(0);
-    }
-
-    private void hashAlgorithm(int min, int max){
-        // Placeholder For Hash Class
-
     }
 
 }
--- src/MD5Hash.java
+++ src/MD5Hash.java
@@ -1,12 +1,32 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+
 public class MD5Hash extends Hash{
 
     public MD5Hash(String hash, int threads, String path, boolean createHashMap) {
         super(hash, threads, path, createHashMap);
     }
 
-    private void hashAlgorithm(int min, int max){
+    private void hashAlgorithm(int min, int max) throws NoSuchAlgorithmException {
         // Algorithm of MD5 Goes Here
-        System.out.println("Min: " + min + "Max:" + max);
+        System.out.println("Right One");
+        for(int x=min;x<=max;x++){
+            MessageDigest MD5 = MessageDigest.getInstance("MD5");
+            byte[] MD5Bytes = MD5.digest(possible[x].getBytes(StandardCharsets.UTF_8));
+            HexFormat MD5Hex = HexFormat.of();
+            String MD5Hash = MD5Hex.formatHex(MD5Bytes);
+            if(MD5Hash.equals(hash)){
+                System.out.println("Found Password: " + possible[x]);
+                stop();
+            }
+        }
+
+    }
+
+    private void h(){
+        System.out.println("Inheritance is good.");
     }
 
 }
